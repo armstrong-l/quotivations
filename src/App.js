54c8f38ded735761621fa71a2dc75a441798b6e7
linda_a <larmstrongnz@gmail.com>
2024-12-17 10:01:37 +1300

    Imported quote data and displayed on screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
+
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+
+
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+    const request = await fetch(quotesUrl);
+    const response = await request.json(); 
+    setQuotes(response);
+  }
+
+catch (e){
+  console.log("Something went wrong", e)
+};
+setLoading(false);
+  };
+
+useEffect(() => {
+  fetchQuotes();
+}, [])
 
   return (
     <div className='App'>
       <Header />
-      <main></main>
+      <main>{loading ? <Loader/> : JSON.stringify(quotes)}</main>
       <Footer />
     </div>
   );
